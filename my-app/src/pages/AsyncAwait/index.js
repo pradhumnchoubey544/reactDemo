a545f91f45c237da3970ebeceb8f2296b324525f
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-06-14 21:36:40 +0530

    add this code

---
+++ my-app/src/pages/AsyncAwait/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ConceptOfPromiseAPI() {
+
+
+    return (
+        <div><h2>Promises</h2>
+            <button title="hello" style={{ width: '20%', height: "100px" }} >Click Me </button>
+        </div>
+    )
+}
