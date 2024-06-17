2ceda153ba97860ff320f919e2eada27bf405d57
pradhumnchoubey544 <125875243+pradhumnchoubey544@users.noreply.github.com>
2024-06-17 14:01:42 +0530

    add this code

--- my-app/src/pages/AsyncAwait/index.js
+++ my-app/src/pages/AsyncAwait/index.js
@@ -9,3 +9,21 @@ export default function ConceptOfPromiseAPI() {
         </div>
     )
 }
+
+
+
+// why us async
+// async is keyword which is used befor a function to create async function this async function always return promise 
+// either you return (promise from this  asyc function) so it will return promise becouse (by-defalut it will return promise)
+// Or you (return anythis string ,number) in this case it will automaticaly rape this return value in promise and return becouse (by-defalut it will return promise)
+
+
+// async and await combo will hendle promises
+//how do we hande promise before in old time 
+//Ex:-      let promsie = fatch("url");
+//          Promise.then(()=>{}) .then(()=>{}) .then(()=>{}) .then(()=>{}) 
+// in this way before old time  we handel promise
+
+
+// await can only be used inside asynce function and you write await before promise and await resolve this promise
+
